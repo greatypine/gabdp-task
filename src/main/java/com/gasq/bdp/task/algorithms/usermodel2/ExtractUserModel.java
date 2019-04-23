a9356477bacbdfa52c754dbab89395180cebe4e1
annerenmian@163.com <annerenmian@163.com>
2019-04-23 17:52:54 +0800

    增加数据集的筛选和计算。合并结果直接输出用户数据模型

--- src/main/java/com/gasq/bdp/task/algorithms/usermodel2/ExtractUserModel.java
+++ src/main/java/com/gasq/bdp/task/algorithms/usermodel2/ExtractUserModel.java
@@ -5,13 +5,18 @@ import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
@@ -22,23 +27,34 @@ import org.slf4j.LoggerFactory;
 
 import com.gasq.bdp.task.GasqSparkTask;
 
+import scala.Tuple2;
+
+/**
+ * 提取用户数据模型，并清洗，去除异常数据值
+ * @author RenMian
+ *
+ */
 public class ExtractUserModel implements GasqSparkTask, Serializable {
 	
+
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -3212332030469538560L;
 	private static final Logger logger = LoggerFactory.getLogger(ExtractUserModel.class);
+	private static final String DELIMER = "\t";
 	
 	public static enum ACTION {
-		Request("request", "f_customer_pro_request_new_action"), 	//请求行为
-		Add("add", "f_customer_pro_add_new_action"), 			//加入购物车
-		SuOrder("suorder", "f_customer_pro_suorder_new_action");		//成功交易订单
+		Request("request", "f_customer_pro_request_new_action", 0.1), 	//请求行为
+		Add("add", "f_customer_pro_add_new_action", 1.0), 			//加入购物车
+		SuOrder("suorder", "f_customer_pro_suorder_new_action", 0.5);		//成功交易订单
 		private String action;
 		private String table;
-		ACTION(String action, String table) {
+		private double alph;
+		ACTION(String action, String table, double alph) {
 			this.action = action;
 			this.table = table;
+			this.alph = alph;
 		}
 		public String getAction() {
 			return this.action;
@@ -46,24 +62,45 @@ public class ExtractUserModel implements GasqSparkTask, Serializable {
 		public String getTable() {
 			return this.table;
 		}
-		
+		public double getAlph() {
+			return this.alph;
+		}
 	}
 	
 	private SparkSession spark;
-	private int taskNum = 3;
+	private int taskNum = 3;	//循环执行三次
+	private Map<String, Double> alphMap;
 	
 	public ExtractUserModel() {
 		if(spark == null) {
 			spark = getHiveSpark("Extract User Model", false);
 			JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
 			spark.conf().set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-			sc.getConf().registerKryoClasses(new Class[]{com.gasq.bdp.task.algorithms.usermodel2.UserCommodityActionBean.class});
+			sc.getConf().registerKryoClasses(new Class[]{com.gasq.bdp.task.algorithms.usermodel2.UserCommodityActionBean.class,
+					com.gasq.bdp.task.algorithms.usermodel2.UserModelBean.class});
+			alphMap = new HashMap<>();
 		}
 	}
 
 	public static void main(String[] args) {
+		if(args == null || args.length == 0 || StringUtils.isBlank(args[0])) {
+			logger.error("arg0 is output path! arg1 is alph param format: request=0.1,add=1,suorder=0.5");
+			return;
+		}
 		ExtractUserModel model = new ExtractUserModel();
 		try {
+			if(args != null && args.length == 2) {
+				String alph = args[1];
+				String[] alphs = alph.split(",");
+				for(int i = 0; i < alphs.length; i++) {
+					if(alphs[i] != null && alphs[i].contains("=")) {
+						String[] str = alphs[i].split("=");
+						if(str != null && str.length == 2 && StringUtils.isNumeric(str[1]))
+							model.alphMap.put(str[0], Double.parseDouble(str[1]));
+					}
+					
+				}
+			}
 			model.run(args);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -81,16 +118,28 @@ public class ExtractUserModel implements GasqSparkTask, Serializable {
 		for(int i = 0; i < actions.length; i++) {
 			Instant start = Instant.now();
 			logger.warn("************begin to deal with::::" + actions[i].getAction());
-			cleanData(actions[i].getTable(), (i == 0));
+			cleanData(actions[i].getTable(), (i==0));
 			Instant end = Instant.now();
 			logger.warn("************end to cost::::" + Duration.between(start, end).toMillis() + "ms");
 		}
+		//--------------------------merge数据并写到hdfs中
+		String sql = "select customer_id, tag_level4_id, sum(score) as score from default.r_customer_pro_new_action group by customer_id, tag_level4_id";
+		Dataset<Row> dataset = spark.sql(sql);
+		dataset.map(row -> {
+			return String.join(DELIMER, row.getString(0), row.getString(1), String.valueOf(row.getDouble(2)));
+		},  Encoders.bean(String.class)).coalesce(1).write().mode(SaveMode.Overwrite).text(args[0]);
 		spark.close();
 		Instant endT = Instant.now();
 		logger.warn("Done work cost=" + Duration.between(startT, endT).toMillis() + "ms");
 		return 0;
 	}
 	
+	/**
+	 * 清洗数据，默认清洗三次
+	 * @param tableName
+	 * @param isfirst
+	 * @throws Exception
+	 */
 	private void cleanData(String tableName, boolean isfirst) throws Exception {
 		String action = getActionBy(tableName);
 		if(StringUtils.isBlank(action)) {
@@ -113,18 +162,48 @@ public class ExtractUserModel implements GasqSparkTask, Serializable {
 			beanSet = normalizeData(beanSet);
 			i++;
 		}
-		beanSet.createOrReplaceTempView("user_model_temp");
 		
-		if(isfirst) {
-			int result = (int)spark.sql("DROP TABLE IF EXISTS default.r_customer_pro_new_action").count();
-//			System.out.println("************Result=" + result);
-			spark.sql("CREATE TABLE IF NOT EXISTS default.r_customer_pro_new_action stored as parquet as select *, '" + action + "' as action from user_model_temp");
-		} else {
-			spark.sql("insert into default.r_customer_pro_new_action select *, '" + action + "' as action from user_model_temp");
+		
+		double alph = getAlphByAction(action);
+		logger.warn("!!!!!!!!get Action[" + action + "] Alph=" + alph);
+		if(alph > 0) {
+			JavaRDD<UserModelBean> beanKV = beanSet.javaRDD().mapToPair(f->{
+				String newkey =  String.join(DELIMER,f.getCustomer_id(),f.getTag_level4_id());
+	        	double vv = f.getScore() * Math.log(1+f.getDiffday()) * alph;
+	        	return new Tuple2<String, Double>(newkey, vv);
+			}).reduceByKey(Double::sum).map(f -> {
+				String[] key = f._1.split(DELIMER);
+				UserModelBean bean = new UserModelBean();
+				bean.setCustomer_id(key[0]);
+				bean.setTag_level4_id(key[1]);
+	//			bean.setAction(action);
+				bean.setScore(f._2);
+				return bean;	
+			});
+			Dataset<Row> newBeanSet = spark.createDataFrame(beanKV, UserModelBean.class);
+			newBeanSet.createOrReplaceTempView("user_model_temp");
+			
+			if(isfirst) {
+				spark.sql("DROP TABLE IF EXISTS default.r_customer_pro_new_action").count();
+	//			System.out.println("************Result=" + result);
+				spark.sql("CREATE TABLE IF NOT EXISTS default.r_customer_pro_new_action stored as parquet as select *, '" + action + "' as action from user_model_temp");
+			} else {
+				spark.sql("insert into default.r_customer_pro_new_action select *, '" + action + "' as action from user_model_temp");
+			}
 		}
+		
+		
+		//复写原有表
+//		spark.sql("insert overwrite table " + tableName + " select request_date, diffday, customer_id, tag_level4_id, score from user_model_temp");
 		beanSet.unpersist();
 	}
 	
+	/**
+	 * 去除数据异常值
+	 * @param beanSet
+	 * @return
+	 * @throws Exception
+	 */
 	private Dataset<UserCommodityActionBean> normalizeData(Dataset<UserCommodityActionBean> beanSet) throws Exception {
 //	    List<Integer> scoreList = beanSet.javaRDD().map(bean->Integer.parseInt(String.valueOf(bean.getScore()))).collect();             
 //		Optional<Integer> sum = scoreList.stream().reduce(Integer::sum);
@@ -152,7 +231,26 @@ public class ExtractUserModel implements GasqSparkTask, Serializable {
 		return beanSet.filter(bean -> bean.getScore() >= min && bean.getScore() <= max);
 	}
 	
-	private String getActionBy(String tableName) {
+	private double getAlphByAction(String action) {
+		Double alph = this.alphMap.get(action);
+		if(alph == null) {
+			if(action.equals(ACTION.Request.getAction())) {
+				alph = ACTION.Request.getAlph();
+			} else if (action.equals(ACTION.Add.getAction())) {
+				alph = ACTION.Add.getAlph();
+			} else if (action.equals(ACTION.SuOrder.getAction())) {
+				alph = ACTION.SuOrder.getAlph();
+			}
+		}
+		return (alph == null) ? 0 : alph;
+	}
+	
+	/*
+	 * 根据table名称获取action
+	 * @param tableName
+	 * @return
+	 */
+	protected String getActionBy(String tableName) {
 		String action = "";
 		if(tableName != null) {
 			if(tableName.equalsIgnoreCase(ACTION.Request.getTable())) {
---
+++ src/main/java/com/gasq/bdp/task/algorithms/usermodel2/UserModelBean.java
@@ -0,0 +1,40 @@
+package com.gasq.bdp.task.algorithms.usermodel2;
+
+import java.io.Serializable;
+
+public class UserModelBean implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5974011142759973555L;
+//	private String action;
+	private String customer_id;
+	private String tag_level4_id;
+	private double score;
+	
+//	public String getAction() {
+//		return action;
+//	}
+//	public void setAction(String action) {
+//		this.action = action;
+//	}
+	public String getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(String customer_id) {
+		this.customer_id = customer_id;
+	}
+	public String getTag_level4_id() {
+		return tag_level4_id;
+	}
+	public void setTag_level4_id(String tag_level4_id) {
+		this.tag_level4_id = tag_level4_id;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+}
